7b0660fd94e9a3dc0d176a2caa4f45e7d56d42b0
Jimme05 <tiwtiwggtv@gmail.com>
2025-03-02 23:42:36 +0700

    111

--- includes/view.php
+++ includes/view.php
@@ -4,8 +4,14 @@ declare(strict_types=1);
 
 function renderView(string $template, array $data = []): void
 {
-    if ($template !== 'home_get' || 'login_get' || 'register_get') {
-        include TEMPLATES_DIR . '/header.php';
+    if ($template !== 'home_get') {
+        if($template !== 'login_get'){
+            if($template !== 'register_get'){
+                include TEMPLATES_DIR . '/header.php';
+            }
+           
+        }
+        
     }
     
     include TEMPLATES_DIR . '/' . $template . '.php';
