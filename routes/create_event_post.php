a7db5075b7e5a0fa8e842d43ba51416f75461b07
Jimme05 <tiwtiwggtv@gmail.com>
2025-03-03 14:09:29 +0700

    123

--- routes/create_event_post.php
+++ routes/create_event_post.php
@@ -1,11 +1,6 @@
 <?php
 // ในไฟล์ create_event_post.php
 
-
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
-
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     if (isset($_SESSION['uid'])) {
         $uid = $_SESSION['uid']; // รับ uid จาก session
