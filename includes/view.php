f2f0ef996cfd845cd2ed2e89c85a8a230a58981c
Jimme05 <tiwtiwggtv@gmail.com>
2025-03-27 19:37:36 +0700

    213

--- includes/view.php
+++ includes/view.php
@@ -7,12 +7,29 @@ function renderView(string $template, array $data = []): void
     if ($template !== 'home_get') {
         if($template !== 'login_get'){
             if($template !== 'register_get'){
-                include TEMPLATES_DIR . '/header.php';
+                if($template !== 'event_scr_get'){
+                    include TEMPLATES_DIR . '/header.php';
+                    
+                }
             }
            
         }
         
     }
     
+    if ($template !== 'home_get') {
+        if($template !== 'login_get'){
+            if($template !== 'register_get'){
+                if (!isset($_SESSION['uid'])) {
+                    echo "<script>alert('Login ก่อน!'); window.location.href='/login';</script>";
+                    exit;
+                }
+            }
+           
+        }
+        
+    }
+    
+     
     include TEMPLATES_DIR . '/' . $template . '.php';
 }
